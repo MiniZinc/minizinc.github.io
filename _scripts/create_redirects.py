11b3bb6ec74e605258765463942b6b1142e7a546
Guido Tack <guido.tack@monash.edu>
2018-10-26 13:05:55 +1100

    Add redirects for Chinese handbook

--- _scripts/create_redirects.py
+++ _scripts/create_redirects.py
@@ -9,3 +9,10 @@ for htmlfile in os.listdir("doc-"+latest+"/en")+["downloads/index.html"]:
             f.write("redirect_to: \"/doc-"+latest+"/en/"+htmlfile+"\"\n")
             f.write("---\n")
 
+for htmlfile in os.listdir("doc-"+latest+"/chi")+["downloads/index.html"]:
+    if htmlfile.endswith(".html"):
+        with open("doc-latest/chi/"+htmlfile,"w") as f:
+            f.write("---\n")
+            f.write("redirect_to: \"/doc-"+latest+"/chi/"+htmlfile+"\"\n")
+            f.write("---\n")
+
