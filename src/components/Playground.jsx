967ca7c045e3672fb9066512c0cdb8a2a12bafc9
Jason N <cyderize@users.noreply.github.com>
2023-11-20 19:48:42 +1100

    Improve playground component

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,145 +1,145 @@
-import { Component } from 'react'
+import { useEffect, useId, useState } from 'react'
 import { Container } from '@/components/Container'
 import { SelectField } from './Fields'
-import AustraliaProject from '@/data/australia.json'
-import NQueensProject from '@/data/nqueens.json'
+import AustraliaProject from '@/data/examples/australia.json'
+import NQueensProject from '@/data/examples/nqueens.json'
+import PackingProject from '@/data/examples/packing.json'
+import SudokuProject from '@/data/examples/sudoku.json'
+import Link from 'next/link'
 
-const examples = [AustraliaProject, NQueensProject]
-const alphabet =
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const examples = [
+  AustraliaProject,
+  NQueensProject,
+  PackingProject,
+  SudokuProject,
+]
 
-export class Playground extends Component {
-  constructor(props) {
-    super(props)
-  }
+export function Playground() {
+  const id = useId()
+  const [playground, setPlayground] = useState(null)
+  const [target, setTarget] = useState(null)
+  const [svelteComponent, setSvelteComponent] = useState(null)
+  const [project, setProject] = useState(examples[0].project)
 
-  componentDidMount() {
-    const id = Array(6)
-      .fill(0)
-      .map(() => alphabet[Math.floor(Math.random() * alphabet.length)])
-      .join('')
-    window[`playground-${id}`] = this
-    if (!this.script) {
-      this.script = document.createElement('script')
-      this.script.type = 'module'
-      this.script.innerHTML = `
+  useEffect(() => {
+    const index = Math.floor(examples.length * Math.random())
+    setProject(examples[index].project)
+    window[`playground-${id}`] = (p) => setPlayground({ playground: p })
+    const script = document.createElement('script')
+    script.id = `playground-script-${id}`
+    script.type = 'module'
+    script.innerHTML = `
         import Playground from 'https://cdn.jsdelivr.net/gh/minizinc/minizinc-playground@library/dist/minizinc-playground.js';
-        window['playground-${id}'].initPlayground(Playground);
-        delete window['playground-${id}'];
-      `
-      this.instance.appendChild(this.script)
-    }
-  }
-
-  initPlayground(SveltePlayground) {
-    if (!this.svelteComponent) {
-      const mediaQuery = matchMedia('(max-width: 768px)')
-      this.svelteComponent = new SveltePlayground({
-        target: this.target,
-        props: {
-          project: examples[0].project,
-          splitterDirection: mediaQuery.matches ? 'vertical' : 'horizontal',
-          splitterSize: 50,
-          showShareButton: false,
-          showDownloadButton: false,
-          compilationEnabled: false,
-          showVersionSwitcher: false,
-          canSwitchOrientation: false,
-          autoClearOutput: true,
-          showClearOutput: false,
-          showAutoClearOutput: false,
-          showOutputSectionToggles: false,
-          showOutputRightControls: false,
-          canEditTabs: false,
-          externalPlaygroundURL: 'https://play.minizinc.dev/',
-          theme: 'light',
-        },
-      })
-
-      const mediaQueryChange = (e) => {
-        if (this.svelteComponent) {
-          this.svelteComponent.$set({
-            splitterDirection: e.matches ? 'vertical' : 'horizontal',
-          })
+        if ('playground-${id}' in window) {
+          window['playground-${id}'](Playground);
+          delete window['playground-${id}'];
+          document.getElementById('playground-script-${id}').remove();
         }
-      }
-      mediaQuery.addEventListener('change', mediaQueryChange)
-      this.destroyMediaQuery = () =>
-        mediaQuery.removeEventListener('change', mediaQuery)
-    }
-    this.script.remove()
-    this.script = null
-  }
+      `
+    document.body.appendChild(script)
+  }, [id])
 
-  mediaChange(e) {
-    if (this.svelteComponent) {
-      this.svelteComponent.$set({
+  useEffect(() => {
+    if (!playground || !target) {
+      return
+    }
+
+    const mediaQuery = matchMedia('(max-width: 768px)')
+    const component = new playground.playground({
+      target,
+      props: {
+        project: examples[0].project,
+        splitterDirection: mediaQuery.matches ? 'vertical' : 'horizontal',
+        splitterSize: 60,
+        showShareButton: false,
+        showDownloadButton: false,
+        compilationEnabled: false,
+        showVersionSwitcher: false,
+        canSwitchOrientation: false,
+        autoClearOutput: true,
+        showClearOutput: false,
+        showAutoClearOutput: false,
+        showOutputSectionToggles: false,
+        showOutputRightControls: false,
+        canEditTabs: false,
+        externalPlaygroundURL: 'https://play.minizinc.dev/',
+        theme: 'light',
+      },
+    })
+
+    const mediaQueryChange = (e) => {
+      component.$set({
         splitterDirection: e.matches ? 'vertical' : 'horizontal',
       })
     }
-  }
-
-  setProject(project) {
-    if (this.svelteComponent) {
-      this.svelteComponent.$set({ project })
+    mediaQuery.addEventListener('change', mediaQueryChange)
+    setSvelteComponent({ component })
+    return () => {
+      mediaQuery.removeEventListener('change', mediaQuery)
+      component.$destroy()
     }
-  }
+  }, [playground, target])
 
-  change(e) {
-    this.setProject(examples[e.target.value].project)
-  }
-
-  componentWillUnmount() {
-    if (this.svelteComponent) {
-      this.destroyMediaQuery()
-      this.svelteComponent.$destroy()
-      this.svelteComponent = null
+  useEffect(() => {
+    if (svelteComponent && project) {
+      svelteComponent.component.$set({ project })
     }
-  }
+  }, [svelteComponent, project])
 
-  render() {
-    return (
-      <section
-        id="playground"
-        aria-labelledby="playground-title"
-        className="border-t border-gray-200 bg-gray-100 py-20 sm:py-32"
-      >
-        <style jsx>{`
-          @import url('https://cdn.jsdelivr.net/gh/minizinc/minizinc-playground@library/dist/style.css');
-        `}</style>
-        <Container>
-          <div className="mx-auto max-w-2xl text-center">
-            <h2
-              id="playground-title"
-              className="text-3xl font-medium tracking-tight text-gray-900"
+  return (
+    <section
+      id="playground"
+      aria-labelledby="playground-title"
+      className="border-t border-gray-200 bg-gray-100 py-20 sm:py-32"
+    >
+      <style jsx>{`
+        @import url('https://cdn.jsdelivr.net/gh/minizinc/minizinc-playground@library/dist/style.css');
+      `}</style>
+      <Container>
+        <div className="mx-auto max-w-2xl text-center">
+          <h2
+            id="playground-title"
+            className="text-3xl font-medium tracking-tight text-gray-900"
+          >
+            Try MiniZinc
+          </h2>
+        </div>
+        <div className="mt-2">
+          <p className="text-center">
+            Try one of the examples, or{' '}
+            <Link
+              className="text-primary hover:text-primary-dark -active:text-primary-light underline"
+              href="https://play.minizinc.dev"
+              target="_blank"
             >
-              Try MiniZinc
-            </h2>
-          </div>
-          <div className="mt-8">
-            <SelectField
-              id="playground-example"
-              className="col-span-full"
-              onChange={Playground.prototype.change.bind(this)}
-            >
-              {examples.map((x, i) => (
-                <option key={i} value={i}>
-                  {x.name}
-                </option>
-              ))}
-            </SelectField>
-          </div>
-          <div className="mt-8" ref={(el) => (this.instance = el)}>
-            <div
-              ref={(el) => (this.target = el)}
-              style={{
-                height: '540px',
-                boxShadow: '0 0 1rem 1rem rgba(0, 0, 0, 0.08)',
-              }}
-            />
-          </div>
-        </Container>
-      </section>
-    )
-  }
+              write your own model
+            </Link>
+            .
+          </p>
+        </div>
+        <div className="mt-8">
+          <SelectField
+            id="playground-example"
+            className="col-span-full"
+            onChange={(e) => setProject(examples[e.target.value].project)}
+          >
+            {examples.map((x, i) => (
+              <option key={i} value={i} selected={x.project === project}>
+                {x.name}
+              </option>
+            ))}
+          </SelectField>
+        </div>
+        <div className="mt-8">
+          <div
+            ref={(el) => setTarget(el)}
+            style={{
+              height: '540px',
+              boxShadow: '0 0 1rem 1rem rgba(0, 0, 0, 0.08)',
+            }}
+          />
+        </div>
+      </Container>
+    </section>
+  )
 }
