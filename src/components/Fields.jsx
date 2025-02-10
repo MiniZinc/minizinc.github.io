b7df654dfda8da7f7b7910ad2367956d6190872e
Jason N <cyderize@users.noreply.github.com>
2025-02-11 10:18:01 +1100

    Update dependencies

--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 
 const formClasses =
-  'block w-full appearance-none rounded-lg border border-gray-200 bg-white py-[calc(theme(spacing.2)-1px)] px-[calc(theme(spacing.3)-1px)] text-gray-900 placeholder:text-gray-400 focus:border-cyan-500 focus:outline-none focus:ring-cyan-500 sm:text-sm'
+  'block w-full appearance-none rounded-lg border border-gray-200 bg-white py-[calc(--spacing(2)-1px)] px-[calc(--spacing(3)-1px)] text-gray-900 placeholder:text-gray-400 focus:border-cyan-500 focus:outline-hidden focus:ring-cyan-500 sm:text-sm'
 
 function Label({ id, children }) {
   return (
