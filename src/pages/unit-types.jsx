8c6e7fa5ebe12bb696d03ad5177d761e675cb304
Jason N <cyderize@users.noreply.github.com>
2025-08-13 22:01:35 +1000

    Update unit types page

--- src/pages/unit-types.jsx
+++ src/pages/unit-types.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Container } from '@/components/Container'
+import { Link } from '@/components/Link'
 
 export default function ObjectsPage() {
   return (
@@ -12,20 +13,25 @@ export default function ObjectsPage() {
       </Head>
       <Header />
       <main>
-      <Container>
-        <section id="objects" aria-labelledby="objects-title">
-          <h2
-            id="objects-title"
-            className="text-3xl font-medium tracking-tight text-gray-900"
-          >
-            Modelling with Unit Types
-          </h2>
-          <hr className="mt-2" />
-          <p className="my-6 leading-7">
-            Coming soon.
-          </p>
-        </section>
-      </Container>
+        <Container>
+          <section id="objects" aria-labelledby="objects-title">
+            <h2
+              id="objects-title"
+              className="text-3xl font-medium tracking-tight text-gray-900"
+            >
+              Modelling with Unit Types
+            </h2>
+            <hr className="mt-2" />
+            <p className="my-6 leading-7">
+              Download the models used for evaluation{' '}
+              <Link href="/unit-types/unit-types.zip">here</Link>.
+            </p>
+            <p>
+              Try the (very early) prototype{' '}
+              <Link href="/unit-types/prototype">here</Link>.
+            </p>
+          </section>
+        </Container>
       </main>
       <Footer />
     </>
