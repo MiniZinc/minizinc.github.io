fe4c9a26037d599eeb737641aaa4479502f37f3c
Jason N <cyderize@users.noreply.github.com>
2026-04-14 11:38:25 +1000

    Remove outlines for Button
    Not sure when this got added, but it looks strange

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ import clsx from 'clsx'
 
 const baseStyles = {
   solid:
-    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors',
+    'inline-flex justify-center rounded-lg py-2 px-3 text-sm font-semibold  transition-colors',
   outline:
-    'inline-flex justify-center rounded-lg border py-[calc(--spacing(2)-1px)] px-[calc(--spacing(3)-1px)] text-sm outline-2 outline-offset-2 transition-colors',
+    'inline-flex justify-center rounded-lg border py-[calc(--spacing(2)-1px)] px-[calc(--spacing(3)-1px)] text-sm transition-colors',
 }
 
 const variantStyles = {
